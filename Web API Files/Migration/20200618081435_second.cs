270b0ad409746d9f001ccc40621c2ad5c0c2e742
Aniket Kadam <kadam007aniket@gmail.com>
2020-06-25 17:25:29 +0530

    Add files via upload

---
+++ Web API Files/Migration/20200618081435_second.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAPI.Migrations
+{
+    public partial class second : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProjectDetailsProjectId",
+                table: "userDetails",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "ProjectDetails",
+                columns: table => new
+                {
+                    ProjectId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ProjectName = table.Column<string>(nullable: false),
+                    ProjectManager = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProjectDetails", x => x.ProjectId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_userDetails_ProjectDetailsProjectId",
+                table: "userDetails",
+                column: "ProjectDetailsProjectId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_userDetails_ProjectDetails_ProjectDetailsProjectId",
+                table: "userDetails",
+                column: "ProjectDetailsProjectId",
+                principalTable: "ProjectDetails",
+                principalColumn: "ProjectId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_userDetails_ProjectDetails_ProjectDetailsProjectId",
+                table: "userDetails");
+
+            migrationBuilder.DropTable(
+                name: "ProjectDetails");
+
+            migrationBuilder.DropIndex(
+                name: "IX_userDetails_ProjectDetailsProjectId",
+                table: "userDetails");
+
+            migrationBuilder.DropColumn(
+                name: "ProjectDetailsProjectId",
+                table: "userDetails");
+        }
+    }
+}
---
+++ Web API Files/Migration/20200622102357_secondmigration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAPI.Migrations
+{
+    public partial class secondmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "registrationDetails",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    UserName = table.Column<string>(nullable: false),
+                    Email = table.Column<string>(nullable: true),
+                    FullName = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: false),
+                    ConfirmPassword = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_registrationDetails", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "registrationDetails");
+        }
+    }
+}
